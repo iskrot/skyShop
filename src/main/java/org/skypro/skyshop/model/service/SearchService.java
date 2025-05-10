7f6de8b4045eb7655c1e26363a150ba53458e333
iskrot <olga.volwskaya@gmail.com>
2025-05-10 20:09:47 +0300

    Update SearchService.java

--- src/main/java/org/skypro/skyshop/model/service/SearchService.java
+++ src/main/java/org/skypro/skyshop/model/service/SearchService.java
@@ -3,6 +3,7 @@ package org.skypro.skyshop.model.service;
 import org.skypro.skyshop.model.search.Searchable;
 import org.springframework.stereotype.Service;
 
+import org.skypro.skyshop.model.search.SearchResult;
 import java.util.Comparator;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -15,12 +16,9 @@ public class SearchService {
         this.storageService = storageService;
     }
 
-    public TreeSet<Searchable> search(String searchString){
-        TreeSet<Searchable> result = storageService.getcollectionSearchable().stream()
+    public TreeSet<SearchResult> search(String searchString){
+        TreeSet<SearchResult> result = storageService.getcollectionSearchable().stream()
                 .filter(i -> i.getSearchTerm().contains(searchString))
+                .map(i -> SearchResult.fromSearchable(i))
                 .collect(Collectors.toCollection(() -> new TreeSet<>()));
         return result;
-    }
-
-
-}
